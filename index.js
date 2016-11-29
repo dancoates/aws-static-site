48a32dff628cb8ee4bf4b7eab7b6bb2e3ed9a4ee
Dan Coates <dan.coates@blueflag.com.au>
2016-11-29 17:29:27 +1100

    some rough updates to deps and arg parsing

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-require('dotenv').config({silent: true});
-const AWS = require('aws-sdk');
+const argv = require('yargs').argv;
+const path = require('path');
 
-const AWSConfig = new AWS.Config({
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+
+require('dotenv').config({
+    silent: true,
+    path: argv.envPath || path.resolve(process.cwd(), '.env')
 });
 
+console.log(process.env.AWS_ACCESS_KEY_ID);
 
-// Create S3 service object
-s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const config = require('./config');
+
+
+
+
+
+
+// const AWS = require('aws-sdk');
+// const AWSConfig = new AWS.Config({
+//     credentials: {
+//         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//     }
+// });
+
+// // Create S3 service object
+// s3 = new AWS.S3({apiVersion: '2006-03-01'});
                     
-// Call S3 to list current buckets
-s3.headBucket({Bucket: 'wowo-dev-test-template'}, (err, data) => {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-});
+// // Call S3 to list current buckets
+// s3.headBucket({Bucket: 'wowo-dev-test-template'}, (err, data) => {
+//     if (err) console.log(err, err.stack); // an error occurred
+//     else     console.log(data);           // successful response
+// });
